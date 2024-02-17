953073c0a21d546f7f0fd6ee2585c7ebf79190aa
dudgh7052 <wkd7052@gmail.com>
2024-02-17 21:19:10 +0900

    Player Animation Update
    Using spriteLibrary, spriteResolver to animation and testing change spriteLibraryAsset

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 public class Player : MonoBehaviour
 {
@@ -14,12 +16,33 @@ public class Player : MonoBehaviour
     /// </summary>
     [SerializeField] float m_moveSpeed;
 
+    public Animator m_animator;
+    public SpriteRenderer m_spriteRenderer;
+    public SpriteLibrary m_spriteLibrary;
+    public SpriteLibraryAsset[] m_spriteLibraryAssets;
+    public int m_index = 0;
+
     void Update()
     {
+        if (m_inputHandler.Movement == Vector2.zero)
+        {
+            m_animator.SetBool("IsMove", false);
+            return;
+        }
+
         Vector2 _moveDir = Vector2.zero;
         _moveDir.x = m_inputHandler.Movement.x;
         _moveDir.y = m_inputHandler.Movement.y;
 
+        m_spriteRenderer.flipX = m_inputHandler.Movement.x > 0 ? false : true; // x 입력에 따라 flipX 해주기
+
+        m_animator.SetBool("IsMove", true); // Animator의 Parameter 변경
         transform.Translate(_moveDir * m_moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        m_index = (m_index + 1) % m_spriteLibraryAssets.Length;
+        m_spriteLibrary.spriteLibraryAsset = m_spriteLibraryAssets[m_index];
     }
 }
